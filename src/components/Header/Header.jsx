05ddac881b40a12e4c6706c61ee217a24deb91eb
Agustin Bejarano <agustin.bejarano98@gmail.com>
2026-08-03 19:22:12 -0300

    feat: inicializar proyecto con estilos globales, Header y separadores

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.css'
+import logoAlvear from '../../assets/img/icon/logoAlvear.png'
+import { Flecha } from './Flecha'
+
+export function Header() {
+    return (
+        <header>
+            <div className="header__layout">
+                <img className="header__img" src={logoAlvear} alt="Escudo de Alvear Club Tenis De Mesa" />
+                <h1 className="header__title">ALVEAR CLUB TENIS DE MESA</h1>
+                <button className='header__button'>
+                    <Flecha />
+                </button>
+            </div>
+        </header>
+    )
+}
---
+++ src/components/Separador/Separador.jsx
@@ -0,0 +1,10 @@
+import './Separador.css'
+
+export function Separador({id}) {
+    return (
+        <div className="separador" id={id}>
+            <div className="separador__layout">
+            </div>
+        </div>
+    )
+}
