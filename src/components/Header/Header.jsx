c9f63dceaabeff713476f900e5c180243a22f7af
Agustin Bejarano <agustin.bejarano98@gmail.com>
2026-08-04 00:54:02 -0300

    feat: agregar funcionalidad al boton flecha del header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,18 @@ import logoAlvear from '../../assets/img/icon/logoAlvear.png'
 import { Flecha } from './Flecha'
 
 export function Header() {
+    const goToInicio =  () => { 
+        const separadorInicio = document.getElementById('inicio');
+        if(separadorInicio) {
+            separadorInicio.scrollIntoView({behavior: 'smooth'})
+        }  
+    }
     return (
         <header>
             <div className="header__layout">
                 <img className="header__img" src={logoAlvear} alt="Escudo de Alvear Club Tenis De Mesa" />
                 <h1 className="header__title">Alvear Club Tenis De Mesa</h1>
-                <button className='header__button'>
+                <button className='header__button' onClick={goToInicio}>
                     <Flecha />
                 </button>
             </div>
